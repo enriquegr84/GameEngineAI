fa1ca9103797b09820779e464f429d9535c5fc1e
Enrique Gonzalez Rodriguez <enriquegr84@hotmail.es>
2020-03-05 12:42:15 +0000

    copyright header

--- Source/GameEngineAI/Quake/QuakeAIManager.h
+++ Source/GameEngineAI/Quake/QuakeAIManager.h
@@ -1,17 +1,9 @@
-//
-//  This program is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU General Public License
-//  as published by the Free Software Foundation; either version 3
-//  of the License, or (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+/*******************************************************
+ * Copyright (C) GameEngineAI - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Enrique González Rodríguez <enriquegr84@hotmail.es>, 2019-2020
+ *******************************************************/
 
 #ifndef QUAKEAIMANAGER_H
 #define QUAKEAIMANAGER_H
